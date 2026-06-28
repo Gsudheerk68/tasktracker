2175e13297b1eb8397443ee5e4c092352f3b2bd5
Gsudheerk68 <sudheersudheerkumar125@gmail.com>
2026-06-28 21:45:39 +0530

    Update taskAPI.js

--- src/services/taskAPI.js
+++ src/services/taskAPI.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://tasktracker-kk9u.onrender.com/api';
+
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
